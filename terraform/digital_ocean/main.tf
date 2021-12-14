0eb23fc6529bdec4753f9b474caf683c64b71cb4
Karl Webster <karlwebster97@gmail.com>
2021-12-14 10:31:52 +0000

    Added digital ocean terraform

---
+++ terraform/digital_ocean/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.16.0"
+    }
+  }
+}
+
+
+resource "digitalocean_kubernetes_cluster" "dokc" {
+  name   = "dokc"
+  region = "lon1"
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.21.5-do.0"
+
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-2vcpu-2gb"
+    node_count = 2
+  }
+}
